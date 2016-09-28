cb51174d4ad564dba6126b3f3abdb40a9f3a450c
Iggi <iggirex@gmail.com>
2016-09-27 21:57:25 -0600

    counting connections

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var io = require('socket.io')()
 
+var connections = [];
+
 io.on("connection", function(socket){
-  console.log("We're iniside initial server receiving message and a USER has CONNECteD3")
-  // io.sockets.emit("THIS IS socket.broadcast.emit");
+  connections.push(socket)
+  console.log("connections length: ", connections.length)
+  console.log("USER has CONNECteD3, and there are %s sockets connected", connections.length )
   socket.on("chat message", function(msg){
     console.log("message ISSZZ:", msg)
     socket.broadcast.emit("chat message", msg)
-  })
+  });
   socket.on("disconnect", function(){
-    console.log("user HAS disCONnected")
+    connections.splice(connections.indexOf(socket), 1);
+    console.log("user HAS disCONnected and there are %s sockets connected", connections.length)
   })
-})
+});
 
 module.exports = io
